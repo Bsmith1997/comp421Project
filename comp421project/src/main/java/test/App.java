d56e921335006b8989c7c2679ecc448c368ae705
Babette Smith <babette.smith@mail.mcgill.ca>
2020-04-11 20:18:44 -0400

    View appointments of nail artist working

--- comp421project/src/main/java/test/App.java
+++ comp421project/src/main/java/test/App.java
@@ -1,9 +1,6 @@
 package test;
 
 import java.sql.*;
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Scanner;
 
 import test.AddUserToRewardProgramController;
@@ -32,14 +29,14 @@ public class App {
 		String name = "John Doe";
 		String useraddress = "100 Fake Street";
 		
-boolean continueLoop1 = true;
+boolean continueLoop = true;
         
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Hello, welcome to the Nail Salon Management System and Database!");
         System.out.println("What would you like to do?");
         
-        while(continueLoop1) {
+        while(continueLoop) {
             
             System.out.println("---------------------------");
             System.out.println("Select one of the following options to continue:");
@@ -63,11 +60,14 @@ boolean continueLoop1 = true;
                     if((userInput >= 1) && (userInput <= 6)) {
                         
                         validInput = true;
+                      
                         
                         if(userInput == 1){
-                          System.out.println("Enter the email address of the nail artist you want to view the appointments of:");
-                          String nailArtistEmail = scanner.nextLine();
-                          LookUpNailArtistController.LookUp(statement, nailArtistEmail);
+                          //continueLoop=false;
+                          LookUp(scanner, statement);
+                          //continueLoop=true;
+                          break;
+                          
                         }
                         
                         else if(userInput == 2){
@@ -88,7 +88,7 @@ boolean continueLoop1 = true;
                         
                         else{
                             System.out.println("Goodbye.");
-                            continueLoop1 = false;
+                            continueLoop = false;
                         }
                     }
                     
@@ -140,5 +140,38 @@ boolean continueLoop1 = true;
 		// Finally but importantly close the statement and connection
 		statement.close();
 		con.close();
+	}
+	
+	public static void LookUp(Scanner scanner, Statement statement) {
+        System.out.println("Enter the email address of the nail artist you want to view the appointments of:");
+        Scanner scanner1 = new Scanner(System.in);
+        if (scanner1.hasNext()){
+          String nailArtistEmail = scanner1.nextLine();
+        if (nailArtistEmail == "" || nailArtistEmail == null) {
+            System.out.println("Nail Artists' email cannot be null or empty");
+            return;
+        } 
+          try {
+        
+          String lookUpSQLInAppointments = "SELECT starttime, cemail FROM  appointment WHERE nartistemail = \'"
+                  + nailArtistEmail + "\'";
+          System.out.println(lookUpSQLInAppointments);
+          ResultSet rs = statement.executeQuery(lookUpSQLInAppointments);
+          while (rs.next()) {
+              // Retrieve by column name
+              String startTime = rs.getString("starttime");
+              String email = rs.getString("cemail");
+
+              // Display values
+              System.out.print("Start Time: " + startTime);
+              System.out.println(", Customer Email: " + email);
+          }
+          System.out.println("DONE");
+
+      } catch (SQLException e) {
+          System.out.println("Error: Failed to execute command");
+          e.printStackTrace();
+          }
+        }
 	}
 }
